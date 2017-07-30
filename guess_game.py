1a38ccdb67f58246b01df4a166c489f706c29cef
shenmemingzine <rongwingyu@gmail.com>
2017-07-29 21:09:32 -0700

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -5,13 +5,16 @@ guess = int(input('Enter an integer between 1 and 99: '))
 
 
 if guess == answer:
-    print('You guessed it!')
+    print('You guessed it with one attempts!')
 else:
+    attempts = 0
     while guess != answer:
         if guess < answer:
             print('Your guess is low!')
             guess = int(input('Enter a new guess: '))
+            attempts += 1
         elif guess > answer:
             print('Your guess is high!')
             guess = int(input('Enter a new guess: '))
-    print('You guessed it!')
+            attempts += 1
+    print('You guessed it with ' + str(attempts + 1) + ' attempts!')
